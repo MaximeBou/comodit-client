dae621e77fcaade01076ad5869b3e97635d98a96
maxime bourdouxhe <mbourdouxhe@willow.angleur.guardis.be>
2016-09-07 17:36:17 +0200

    Add scripts for deb & rpm building job

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -5,21 +5,11 @@ TMP_DIR=/tmp/comodit-client
 
 cd `dirname $0`
 cd ..
+
 # Set version information
-VERSION=`git describe --tags --long  | awk -F"-" '{print $2}'`
-RELEASE=`git describe --tags --long | awk -F"-" '{print $3}'`
-COMMIT=`git describe --tags --long | awk -F"-" '{print $4}'`
-MESSAGE="Release $VERSION-$RELEASE-$COMMIT"
-
-echo $MESSAGE
-
-export DEBEMAIL
-export DEBFULLNAME
-
-debchange --newversion $VERSION-$RELEASE "$MESSAGE"
-
-unset DEBEMAIL
-unset DEBFULLNAME
+. scripts/build-pkg-functions
+set_version $1 $2
+generate_version_file $VERSION $RELEASE
 
 # Build package
 DIST_DIR=${TMP_DIR}/dist
